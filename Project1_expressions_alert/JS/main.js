b66e8d6fbf3328a60d96596dc86ce05e94cd4347
JagdeepSaran <saranjagdeep66@gmail.com>
2025-09-10 12:08:25 -0400

    Added Project1_expressions_alert

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+// window.alert method
+window.alert("Hello, this is our new assignment!");
+
+// document.write method
+document.write("We are learning JavaScript with simple examples.<br>");
+
+// creating two variables
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+
+// displaying the variables using the document.write method
+document.write(Sent1 + Sent2 + "<br>");
+
+// an expression
+document.write(10 + 25);
